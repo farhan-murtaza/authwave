c005ac88c47c170f8191497fcb320d03130dd25c
farhan-murtaza <farhanmurtza03@gmail.com>
2024-08-15 22:36:27 +0500

    Add Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,36 @@
-import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/ui/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
-  return <h1>page</h1>;
+  return (
+    <main
+      className={cn(
+        "flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-sky-800",
+        font.className
+      )}
+    >
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl  font-semibold text-white drop-shadow-md">
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">
+          A simple authentication system using Next.js and Next Auth.
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }

15ae5600e14f71b5ea48f1ead4da44b203653ff7
farhan-murtaza <farhanmurtza03@gmail.com>
2024-08-19 15:34:04 +0500

    Add Google and Github Provider

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -3,6 +3,7 @@ import * as z from "zod";
 import { useState, useTransition } from "react";
 import { CardWrapper } from "./card-wrapper";
 import { useForm } from "react-hook-form";
+import { useSearchParams } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LoginSchema } from "@/schemas";
 import { Input } from "@/components/ui/input";
@@ -21,6 +22,11 @@ import { FormSuccess } from "../form-success";
 import { login } from "@/actions/login";
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get("error") === "OAuthAccountNotLinked"
+      ? "Email already in use with different provider!"
+      : "";
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [isPending, startTransition] = useTransition();
@@ -37,8 +43,9 @@ export const LoginForm = () => {
     setSuccess("");
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        // TODO: Add when we add 2FA
+        // setSuccess(data?.success);
       });
     });
   };
@@ -94,7 +101,7 @@ export const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
           <Button disabled={isPending} type="submit" className="w-full">
             Login
---
+++ next-auth.d.ts
@@ -0,0 +1,19 @@
+import { UserRole } from "@prisma/client";
+import { DefaultSession } from "next-auth";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role?: UserRole;
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
+
+import { JWT } from "@auth/core/jwt";
+declare module "@auth/core/jwt" {
+  interface JWT {
+    role?: UserRole;
+  }
+}
